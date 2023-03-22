b3828972074fe93e3f8b7eeb8310388aa43eeb8f
Lasse Lykke Jensen <lasselj@yandex.com>
2023-03-22 17:27:01 +0100

    Fix raycasting

--- src/src/VoxelGame/Player.cpp
+++ src/src/VoxelGame/Player.cpp
@@ -5,16 +5,27 @@
 #include "glcore/Shader.h"
 #include "glm/fwd.hpp"
 #include "glm/geometric.hpp"
+#include <glm/gtx/string_cast.hpp>
 
 namespace VoxelGame {
+
+    template<typename T>
+    int sgn(T val) {
+	return (T(0) < val) - (val < T(0));
+    }
+
   void Player::CastRay(const Map* RaycastMap) {
     glm::vec3 RayMovement = PlayerCam.Front / (float)RaycastQuality;
     glm::vec3 CurrentRaycastPosition = PlayerCam.Position;
+    CurrentRaycastPosition += glm::vec3{
+	    sgn(CurrentRaycastPosition.x) * 0.5,
+	    sgn(CurrentRaycastPosition.y) * 0.5,
+	    sgn(CurrentRaycastPosition.z) * 0.5};
 
     for(int i = 0; i < RayDistance * RaycastQuality; i++) {
       CurrentRaycastPosition += RayMovement;
 
-      glm::vec3 BlockPos = CurrentRaycastPosition - glm::vec3{0.5, -0.5, 0.5};
+      glm::ivec3 BlockPos = CurrentRaycastPosition;
       if(RaycastMap->GetBlockID(BlockPos.x, BlockPos.y, BlockPos.z)) {
 	RayHit = true;
 	LookRayHitLoc = BlockPos;
